231d057737e02ba5bf9a2de47f1e429ad8585df0
Daniel Larsen <daniel@larsen.net.nz>
2015-04-12 20:05:07 +1200

    API App Host swallows exception messages? Returning expected Error messages with HTTP 500 responses.

--- ImageResizer/ImageResizer.Api/Controllers/ResizerController.cs
+++ ImageResizer/ImageResizer.Api/Controllers/ResizerController.cs
@@ -10,14 +10,33 @@ using ImageResizer;
 
 namespace ImageResizer.Api.Controllers
 {
+    /// <summary>
+    /// Resizes images in Blob Storage.
+    /// </summary>
     public class ResizerController : ApiController
     {
-        // POST api/values
-        public async Task<string> ResizeInStorage(ResizeTo @params)
+        /// <summary>
+        /// Re-sizes a JPEG image that is stored in Blob Storage to a max width and height, maintaining aspect ratio.
+        /// </summary>
+        /// <param name="params">A JSON or XML model object that must contain uri (string), maxWidth (int) and maxHeight (int).</param>
+        /// <returns>The URL of the new, resized JPEG image.</returns>
+        /// <remarks>Original image Blob must be in the Storage Account that is configured in appSettings. Resized image will be saved
+        /// in the same Container. If the resized image already exists, it will be overwritten.</remarks>
+        public async Task<HttpResponseMessage> ResizeInStorage(ResizeTo @params)
         {
             string uri = @params.Uri;
             int maxWidth = @params.MaxWidth;
             int maxHeight = @params.MaxHeight;
+            string storageAccount = System.Configuration.ConfigurationManager.AppSettings["AZURE_STORAGE_ACCOUNT"];
+
+            // checks
+            if (!uri.EndsWith(".jpg", StringComparison.InvariantCultureIgnoreCase))
+                return ErrorResponse(new NotSupportedException("Only JPEG (.jpg) Images are supported."));
+            if (string.IsNullOrEmpty(storageAccount)) return ErrorResponse(new InvalidOperationException("AZURE_STORAGE_ACCOUNT was not found in AppSettings."));
+            if (!uri.Contains(storageAccount)) 
+                return ErrorResponse(new InvalidOperationException("Source Image blob must be in the Storage account configured for this API."));
+            if (maxWidth < 0 || maxWidth > 10000) return ErrorResponse(new ArgumentOutOfRangeException("maxWidth"));
+            if (maxHeight < 0 || maxHeight > 10000) return ErrorResponse(new ArgumentOutOfRangeException("maxHeight"));
 
             // download the blob
             var storage = new AzureBlobStorage(System.Configuration.ConfigurationManager.AppSettings);
@@ -30,14 +49,18 @@ namespace ImageResizer.Api.Controllers
             //TODO: smelly new filename logic
             var imageUri = new Uri(uri);
             var paths = imageUri.AbsolutePath.Split('/');
-            var newBlobName = paths[paths.Length - 1].Replace(".jpg", string.Format("_{0}_{1}.jpg", maxWidth, maxHeight));
+            var newBlobName = paths[paths.Length - 1].Replace(".jpg", string.Format("_{0}.jpg", Math.Max(maxWidth, maxHeight)));
 
             // save the blob
-            
             var newUri = await storage.Upload(blob.ContainerName, newBlobName, "image/jpeg", newImage);
 
             // return the new URI
-            return newUri;
+            return Request.CreateResponse(HttpStatusCode.OK, new { newUri });
+        }
+
+        private HttpResponseMessage ErrorResponse(Exception exception)
+        {
+            return Request.CreateResponse(HttpStatusCode.InternalServerError, new { message = exception.Message });
         }
     }
 
